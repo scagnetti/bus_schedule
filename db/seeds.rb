b09e0821166ffbfee203cb626010db009a857b71
scagnetti <scagnetti@gmx.net>
2016-06-05 00:49:20 +0200

    Initial Data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-# insert into users('email', 'password_digest') values('volker', '$2a$10$G9sPxTMLXoGMdu0P346WbOVJmMxvfVMWySHt.M/AGzjVhpOvbZp9S');
-User.create(email: "volker", password_digest: "$2a$10$G9sPxTMLXoGMdu0P346WbOVJmMxvfVMWySHt.M/AGzjVhpOvbZp9S")
+User.create!([
+  {email: "volker", password_digest: "$2a$10$q2njUeCM7rZFVBT.sdcTSOIAz4QNAOMl3q4i0nzfGVBoMIr5kuToG"}
+])
+BusStop.create!([
+  {display_name: "Graß", search_name: "Regensburg, Graß", direction_id: 1},
+  {display_name: "Brunnstraße", search_name: "Regensburg, Graß Brunnstraße", direction_id: 1},
+  {display_name: "Fernsehsender Ziegetsdorf", search_name: "Regensburg, Fernsehsender Ziegetsdorf", direction_id: 1},
+  {display_name: "Graßer Weg", search_name: "Regensburg, Graßer Weg", direction_id: 1},
+  {display_name: "Karl-Stieler-Straße", search_name: "Regensburg, Karl-Stieler-Straße", direction_id: 1},
+  {display_name: "Geibelplatz", search_name: "Regensburg, Geibelplatz", direction_id: 1},
+  {display_name: "Wolfgangschule Süd", search_name: "Regensburg, Wolfgangschule Süd", direction_id: 1},
+  {display_name: "Theodor-Storm-Straße", search_name: "Regensburg, Theodor-Storm-Straße", direction_id: 1},
+  {display_name: "Augsburger Straße", search_name: "Regensburg, Augsburger Straße", direction_id: 1},
+  {display_name: "Theresienkirche", search_name: "Regensburg, Theresienkirche", direction_id: 1},
+  {display_name: "Justizgebäude", search_name: "Regensburg, Justizgebäude", direction_id: 1},
+  {display_name: "Bismarckplatz", search_name: "Regensburg, Bismarckplatz", direction_id: 1},
+  {display_name: "Arnulfsplatz", search_name: "Regensburg, Arnulfsplatz", direction_id: 1},
+  {display_name: "Keplerstraße", search_name: "Regensburg, Keplerstraße", direction_id: 1},
+  {display_name: "Fischmarkt", search_name: "Regensburg, Fischmarkt", direction_id: 1},
+  {display_name: "Thundorferstraße", search_name: "Regensburg, Thundorferstraße", direction_id: 1},
+  {display_name: "Dachauplatz", search_name: "Regensburg, Dachauplatz", direction_id: 1},
+  {display_name: "Albertstraße", search_name: "Regensburg, HBF/Albertstraße", direction_id: 1}
+])
+Direction.create!([
+  {display_name: "Schwabenstraße", search_name: "Regensburg Schwabenstraße", line_id: 5},
+  {display_name: "Karl-Stieler-Straße", search_name: "Regensburg Karl-Stieler-Straße", line_id: 5}
+])
+Line.create!([
+  {name: "Linie2AB"}
+])
